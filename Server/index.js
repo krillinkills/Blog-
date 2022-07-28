b48b87b7da62cbc24f4e19496769034aa4354780
krillinkills <beingvaibhav858@gmail.com>
2022-07-28 23:44:23 +0530

    #upload&categories

--- Server/index.js
+++ Server/index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
 const app = express();
 const dotenv = require('dotenv');
 const authRoute = require('./api/routes/auth');
 const userRoute = require('./api/routes/user');
 const postRoute = require('./api/routes/post');
+const catRoute = require('./api/routes/categories');
+const path = require('path');
 
 app.use(express.json());
+app.use(fileUpload());
 dotenv.config();
 
 app.get('/', (req, res) => {
@@ -21,5 +25,22 @@ mongoose
 app.use('/api/auth', authRoute);
 app.use('/api/user', userRoute);
 app.use('/api/post', postRoute);
+app.use('/api/category', catRoute);
+
+//Serve files
+app.use('/images', express.static(path.join(__dirname, '/uploads/')));
+
+///Uplaod files
+app.post('/api/upload', (req, res) => {
+  const fileName = Date.now() + '_' + req.files.file.name;
+  const file = req.files.file;
+  let uploadPath = __dirname + '/uploads/' + fileName;
+  file.mv(uploadPath, (err) => {
+    if (err) {
+      return res.json(err);
+    }
+  });
+  res.json('Uploaded');
+});
 
 app.listen(process.env.PORT, () => console.log('server running'));
